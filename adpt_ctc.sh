db5d0d9346ef1a18ec2460e79d841d9810fabb8f
akio-kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2020-11-02 10:29:17 +0900

    minor revision *sh

--- adpt_ctc.sh
+++ adpt_ctc.sh
@@ -2,8 +2,8 @@
 
 #export CUDA_VISIBLE_DEVICES=$device
 
-train=valid_50.h5
-valid=eval_10.h5
+train=tensorflow_ctc_example_data/valid_50.h5
+valid=tensorflow_ctc_example_data/eval_10.h5
 
 n_labels=82
 
@@ -16,13 +16,14 @@ lstm_depth=3
 batch_size=10
 epochs=5
 learn_rate=1.0e-5
-weights=model/snapshot.h5
+weights=tensorflow_ctc_example_data/model/snapshot.h5
 
 snapdir=./out/
 mkdir -p $snapdir
 mkdir -p tensorboard
 
 python ctc_lstm.py --data $train --valid $valid \
-       --feat-dim $feat_dim --n-labels $n_labels --batch-size $batch_size --epochs $epochs \
+       --feat-dim $feat_dim --n-labels $n_labels \
+       --batch-size $batch_size --epochs $epochs \
        --snapshot $snapdir  --learn-rate $learn_rate \
-       --units $units --lstm-depth $lstm_depth --weights model/snapshot.h5
+       --units $units --lstm-depth $lstm_depth --weights $weights
--- eval_ctc.sh
+++ eval_ctc.sh
@@ -2,7 +2,7 @@
 
 #export CUDA_VISIBLE_DEVICES=$device
 
-data=eval_10.h5
+data=./tensorflow_ctc_example_data/eval_10.h5
 
 # number of labels w/o blank
 n_labels=82
@@ -15,8 +15,7 @@ lstm_depth=3
 #training
 batch_size=1
 learn_rate=1.0e-5;
-snapdir=./out
-model=$snapdir/snapshot.h5
+model=out/snapshot.h5
 python eval_ctc_lstm.py --data $data \
        --feat-dim $feat_dim --n-labels $n_labels --batch-size $batch_size \
        --units $units --lstm-depth $lstm_depth --weights $model \
